08497869a795cd8c694e4df0f6f2ca6360d7521d
Saran <113179649+Santosaran@users.noreply.github.com>
2023-11-14 23:50:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Initial References
 let movieNameRef = document.getElementById("movie-name"); 
-let searchBtn = document.getElementById("search-btn"); 
+let searchBtn = document.getElementById("search-btn");
 let result = document.getElementById("result");
 
 //Function to fetch data from API
