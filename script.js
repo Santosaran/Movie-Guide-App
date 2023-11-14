1184caef61143b3db977fb873e34a6806d2cc61f
Saran <113179649+Santosaran@users.noreply.github.com>
2023-11-14 23:50:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Initial References
-let movieNameRef = document.getElementById("movie-name");
+let movieNameRef = document.getElementById("movie-name"); 
 let searchBtn = document.getElementById("search-btn");
 let result = document.getElementById("result"); 
 
