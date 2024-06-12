b228479c0eee7cd00febc90e6eff605f1b021585
Saran <113179649+Santosaran@users.noreply.github.com>
2024-06-12 23:10:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let result = document.getElementById("result");
 let getMovie = () => {
   let movieName = movieNameRef.value;
   let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
-  //If input field is empty 
+  //If input field is empty
   if (movieName.length <= 0) {
     result.innerHTML = `<h3 class="msg">Please Enter A Movie Name</h3>`;
   }
